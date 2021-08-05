ff9574f46f640e3c63b0dbb42eea6ca97a3dbfd4
andipasti <philippe.nahlik@gmail.com>
2021-08-05 21:50:08 +0200

    angular routings

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LoginsignupComponent } from './components/loginsignup/loginsignup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+
+  {
+    path:'signup',
+    component: LoginsignupComponent
+  },
+
+  {
+    path:'profile',
+    component: ProfileComponent
+  },
+
+  {
+    path:'request-password-reset',
+    component: RequestResetComponent
+  },
+  {
+    path:'response-password-reset',
+    component: ResponseResetComponent
+  },
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
